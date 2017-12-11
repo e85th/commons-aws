85d863eb75b13d2fdafb651259fb41837df4f918
Amar Mehta <xfthhxk@gmail.com>
2017-12-10 21:26:21 -0500

    bump deps

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject e85th/commons-aws "0.1.6-alpha2"
+(defproject e85th/commons-aws "0.1.6-alpha3"
   :description "AWS implementation of protocols in commons"
   :url "http://github.com/e85th/commons-aws"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.9.0-beta3" :scope "provided"]
-                 [e85th/commons "0.1.29-alpha1"]
+                 [e85th/commons "0.1.29-alpha3"]
                  ;; -- AWS
                  [amazonica "0.3.111" :scope "provided"]]
 
